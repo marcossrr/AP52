88e08da9c3fdd68bda6a8b03a20a3fd9528315ad
Marcos <mariro@alumnatflorida.es>
2026-01-29 09:35:48 +0100

    Ejercicio 2

--- 2.sh
+++ 2.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-while raed lista
+contadorL=0
+contadorW=0
+procL=0
+procW=0
+
+while read linea
     do  
-    
+    col2=`echo $linea | awk '{print $2}'`
+    col3=`echo $linea | awk '{print $3}'`
+        if [ $col2 = "Linux" ] 
+        then
+        contadorL=$((contadorL+1))
+        procL=$((procL+col3))
+        else
+        contadorW=$((contadorW+1))
+        procW=$((procW+col3))
+        fi
+    done < listado.txt
 
-
-done < listado.txt
+    echo "Linux-> $contadorL $procL"
+    echo "Windows-> $contadorW $procW"
